b0a1d10d697b208323e852f25167e55b2a583deb
beatit22 <erkankupcu@gmail.com>
2025-07-23 21:38:23 +0300

    Add files via upload

--- konu06/main.js
+++ konu06/main.js
@@ -1,4 +1,4 @@
-/* bir değişkene birden fazla değer ataması yapabilmek için kullanılan gelişmiş veri/data türüdür.
+/* Array: bir değişkene birden fazla değer ataması yapabilmek için kullanılan gelişmiş veri/data türüdür.
 **Her elemanın bir sırası (index) vardır.
 **Javascriptte 0 dan başlar.
 **Veriler üzerinde döngülerle işlem yapmak.
@@ -9,14 +9,16 @@ b) let degiskenAdi = ['parametre1', 'parametre2', 'parametre3']
 Fonksiyonlar hariç tüm data türlerini parametre olarak alabilirler.
 
 **birbirinden farklı veri türlerini diziler sayesinde bir arada tanımlayabiliriz.
-**Sınırsız sayede parametre alabilirler. Büyüyüp küçülebilen verilerdir.
+**Sınırsız sayıda parametre alabilirler. Büyüyüp küçülebilen verilerdir.
 Her elemanın kendine ait bir index no'su vardır.
 Bir dizi verisi içindeki herhangi bir parametreye ulaşmak için o parametrenin index numarasına ihtiyacımız vardır.
 
 
-dizilere ilişlin bir döngü oluşturlacaksa for of döngüsü kullanılmalı. 
+dizilere ilişlin bir döngü oluşturlacaksa "for of döngüsü" kullanılmalı. 
 
+iterable (iterate: yinelenebilir, tekrarlanabilir, üzerinde dolaşılabilir) array(dizi), string (karakter dizisi), set, map
 
+dizi/array, string gibi koleksiyonların her bir elemannı kolay almak için kullanılır. İndeksle uğraşmadan elemanlara doğrudan ulaşım sağlar. 
 */
 
 
@@ -66,3 +68,11 @@ for(let i=0;i<yazdi1.length;i++)
     }
 
 // a etiketine class yazalım. 
+
+const fruits = ['elma', 'armut', 'muz'] /// “fruits adında bir sabit değişken tanımla ve içine bir dizi ata: elma, armut, muz.”
+
+for (const fruit of fruits) // “fruits dizisinin her bir elemanını sırayla fruit adında bir değişkene ata ve her adımda bu fruit değişkenini ekrana yazdır.”
+
+{ 
+    console.log(fruit);
+}
