4985fd85ad1749433ed57d8f82fe69100561c429
beatit22 <erkankupcu@gmail.com>
2025-07-28 21:25:19 +0300

    Add files via upload

--- konu07/main.js
+++ konu07/main.js
@@ -1,8 +1,10 @@
-let yazdi1 = ['Javascript', 'C#', 'PHP', 'Phyton'];
-for (let a of yazdi1) {
-    document.writeln(`<a href="https://eksisozluk.com/" target="_blank" class="dil-link">${a}</a>`);
+let yazdi1 = ['Javascript', 'C#', 'PHP', 'Python'];
+for (let a of yazdi1) { // her bir elemanı a sırayla döner.
+    document.writeln(`<a href="https://eksisozluk.com/" target="_blank" class="erkan">${a}</a>`); // document writeln html ye yazdırır.  
+    // //${a}: Dizi elemanını HTML içinde gösterir.
+    // document: JavaScript’te HTML sayfasının tamamını temsil eden nesnedir (DOM = Document Object Model).
+    // writeln: write + ln (line) = yeni satır anlamına gelir.
 }
-
 
 // arrays metots
 
@@ -46,6 +48,7 @@ let dersBul = function () {
     if (indisNo == 0) {
         alert("aradığınız java dersi bulundu");
         window.location.href = 'https://www.w3schools.com/js/default.asp';
+        //windwo.open yan sekmede açar. window.open('https://www.w3schools.com/js/default.asp', '_blank');
     }
     else if (indisNo == 1) {
         alert("aradığınız php dersi bulundu");
@@ -74,46 +77,171 @@ shift{} BAŞTAN ÇIKARIR. dizi data içindeki ilk elemanı çıkarır.
 unshift{} başa ekler, kaydırır. 
 */
 
-let rez = () => {
 
-    let liste[]
+let liste = []; // boş bir dizi oluşturur. bu diziye misaifr isimleri eklenecektir.
 
-    while (true)
-        
-        {
-        let islem = prompt('yeni kayıt için 1 e basınız', 'silme işlemi için 2ye basınız', 'listeleme 3', 'Çıkış için 4e basınız')
-      
-      
-        if (islem == 1)
-            
-            {
+let rez = () => { // arrow function ile tanımlanmış fonksiyon, çağırıldığında çalışır.
+
+    while (true) { // while ile sonsuz döngü kullanarak kullancıdan sürekli işlem alır. 
+        // bu döngü break ifadesi ile durdurulana kadar sonsuz şekilde çalışır.
+
+        // while (x<10) { x++; } // x birer artarak çalışır. ta ki x küçüktür 10 şartını sağlamayana kadar.
+
+        let islem = prompt('Yeni kayıt için: 1\nSilme için: 2\nListeleme: 3\nÇıkış: 4');
+
+
+        if (islem == 1) {
             let isim = prompt("misafir")
-            liste.push(isim)
+            liste.push(isim) // push ile diziye eklenir. burdaki liste yukarıdaki boş dizidir.
             alert('yeni misafir eklendi')
         }
 
-        
-        else if (isim == 2)
-            {
-            liste.shift()
-        alert('ilk misafir listeden çıkarıldı')
+        else if (islem == 2) {
+            liste.shift() // shift ile listedeki ilk kişi silinir.
+            alert('ilk misafir listeden çıkarıldı')
+        }
+
+        else if (islem == 3) {
+            console.log(liste) // mevcut liste console a yazıdırılır.
+        }
+
+        else if (islem == 4) {
+            alert('dükkan kapandı')
+            break // break ile döngü biter, fonksiyon kapanır.
+        }
+
+        else {
+            alert('yanlış giriş yaptınız') // 1-4 dışındaki girişlerde uyarı verir.
+        }
+    } // if conditional statements bitti
+}; // while -control structrures -- döngü yapıları -- döngü kontrol yapıları (looping / iteration structures) kod bloğunu birden fazla kez çalıştırmak için kullanılır.
+
+
+// iki tane metot. günlük hayatta sahibinden.com'da  konut seçtik, satılık seçti listeliyor, daire rezidans, müstakil ev, fiayatın üzerine tıkladığımızda, büyükten küçüğe tekrar tıkladığında en düşükten yükseğe doğru sıralıyor.
+//1-REVERSE METOTU:
+//bir dizi data içindeki elemanları bazı kaynaklarda parametre olarak geçer. parametreleri sondan başa doğru sıralar. 
+
+let isimler = ['ahmet', 'mahmut', 'gamze', 'berk']
+
+console.log(isimler.reverse()) // baştan sona doğru sırala.
+
+// sort() bir dizi data içindeki ögeleri ,artan yönde veya a dan z ye 
+console.log(isimler.sort())
+console.log(isimler.sort().reverse())
+
+let sayilar = [25, 560, 41, 12, 85]
+console.log(sayilar.sort())
+console.log(sayilar.sort().reverse())
+
+/// z den a ya sıralama yapan bir metot yok. ya da azalan yönde bir sıralama javascriptte yoktur. 
+
+let yenisıra = isimler.sort() // a dan z ye sıralar
+console.log(yenisıra)
+console.log(yenisıra.reverse()) // tersten sıralayarak souncu z den a ya sıralanmış bir şekilde yazar.
+
+/*
+obje datalar 
+-obje datalar {} içinde yazılır. dizilerden farklı olarak key value ikişisi halinde birden fazla değeri 
+
+birden fazla değeri taşıyabilir.
+-dizilerden farklı olarak fonksiyon, eğer bir fonksiyon obje data içinde tanımlanıyorsa buna metot denir. 
+// -fonksiyon ile metot arasındaki fark: metotlar bir nesneye ihtiyaç duyarlar. 
+
+json datalar, 
+
+tırnak kullanımı aharicinde obje data kuralları aynıdır.
+json uzantılı dosyaya yazılırken OBJE DATALAR .js dosyası içine yazılır. içinde bulunduğumuz main.js obje datalar yazılırken, json datalarını bir dosya uzantısı isterler.
+
+yazıcımlar için faydalı apiler. yasin sunmaz
+https://www.yasinsunmaz.com/en-faydali-apiler-yazilimcilar-icin.html?ysclid=mdncdn67jn140286350
+
+http://universities.hipolabs.com/search?country=Turkey
+*/
+
+let uye = {
+    isim: 'Erkan', //1. eleman
+    soyad: 'Küpçü', // 2. eleman
+    yas: 42, // 3. eleman
+    meslek: 'Formateur en marketing digital',
+    egitim: ['Javascript', 'HTML', 'webtasarım', 'Python'], // dizi data olarak verebiliriz.
+    aile: [
+        { kardes: 'Uğur', yas: 46 },
+        { anne: 'Ayşe', yas: 65 },
+        { baba: 'Kemal', yas: 73 }
+    ],
+    //log: function(){alert('merhaba')}
+    login:()=>{
+        let userName = prompt('Kullanıcı adını gir')
+        let sifre = prompt('şifrenizi giriniz')
+        if(userName == uye.user && sifre==uye.pass){ // if (userName==this.user) içinde bulunduğun user anahtarını al. && sifre==this.pass
+            console.log(`${uye.yas} yaşındaki ${uye.meslek} mesleği sahibi ${uye.isim} ${uye.soyad} ; ${uye.egitim.join(', ')} dersleri vermektedir. Annesi ${uye.aile[1].anne} hanım ${uye.aile[0].yas}.`);
+        } else{
+            console.log('yanlış kullanıcı')
+        }
+
+    },
+    user: 'gizem',
+    pass:123,
+    selam:()=> {
+        alert('merhaba')
     }
-
-else if (islem == 3) 
-    {
-    console.log(liste)
-} 
-
-else if (islem == 4)
-    {
-    alert('dükkan kapandı')
-    break
-} 
-
-else
-     {
-    alert('yanlış giriş yaptınız')
-} 
-
-
+   
+    
 }
+
+
+uye.selam()
+
+console.log(uye.isim)
+console.log() // konsol obje içindeki log metodu
+// console.log(uye.log()) 
+uye.login()
+
+console.log(uye.egitim[2]) // üye object datasının eğitim metodu yazılır.
+console.log(uye.aile[1]) // üye obje datasının aile dizi datasında 1.indisteki yaş objesi.
+
+// console.log(uye.aile[1].yas)
+
+/* 
+
+42 yaşındaki eğitmen Erkan Küpçü, javascirpt, html, web tasarım ve python dersleri vermektedir. Annesi Ayşe Hanım, babası Kemal Bey ve kardeşi Uğur Bey'dir. bektik kullanılacak.
+*/
+
+/*  aşağıdaki daha efektif: 
+aile: {
+  kardes: { isim: 'Uğur', yas: 46 },
+  anne: { isim: 'Ayşe', yas: 65 },
+  baba: { isim: 'Kemal', yas: 73 }
+}
+*/
+/* console.log(`${uye.yas} yaşındaki ${uye.meslek} mesleği sahibi ${uye.isim} ${uye.soyad} ; ${uye.egitim.join(', ')} dersleri vermektedir. Annesi ${uye.aile[1].anne} hanım ${uye.aile[0].yas}.`);
+ */
+
+
+/* 
+let uye = {
+    isim: 'Erkan',
+    soyad: 'Küpçü',
+    yas: 42,
+    meslek: 'Formateur en marketing digital',
+    egitim: ['Javascript', 'HTML', 'web tasarım', 'Python'],
+    aile: {
+        kardes: { isim: 'Uğur', yas: 46 },
+        anne: { isim: 'Ayşe', yas: 65 },
+        baba: { isim: 'Kemal', yas: 73 }
+    },
+    log: function () {
+        alert('Merhaba');
+    }
+};
+
+// Tanıtım metni
+console.log(`${uye.yas} yaşındaki eğitmen ${uye.isim} ${uye.soyad}, ${uye.egitim.join(', ')} dersleri vermektedir.`);
+
+// Aile bilgilerini döngüyle yazdır
+for (let rolu in uye.aile) {
+    let kisi = uye.aile[rolu];
+    console.log(`${rolu}: ${kisi.isim}, Yaş: ${kisi.yas}`);
+}
+
+*/
